2a474f85af620e5abe8bb34c44b5f4c110345608
Bharat V <vaidhyanathan.b@husky.neu.edu>
2017-01-30 23:54:38 -0500

    implemented the listview along with the list adapter; setting up of edit text along with keyboard pushing the screen up fixed

--- app/src/main/java/edu/neu/madcourse/bharatvaidhyanathan/assignmentThree/TestDictionaryActivity.java
+++ app/src/main/java/edu/neu/madcourse/bharatvaidhyanathan/assignmentThree/TestDictionaryActivity.java
@@ -3,6 +3,11 @@ package edu.neu.madcourse.bharatvaidhyanathan.assignmentThree;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import edu.neu.madcourse.bharatvaidhyanathan.R;
 
@@ -15,6 +20,18 @@ public class TestDictionaryActivity extends AppCompatActivity {
 
         //setting the title
         setTitle("Test Dictionary");
+
+        //initializing the string array
+        ArrayList<String> matchesStringArray = new ArrayList<>();
+
+        //setting up the array adapter
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, matchesStringArray);
+
+        //Binding the listview to the adapter
+        ListView listView = (ListView) findViewById(R.id.dictionaryListView);
+        listView.setAdapter(arrayAdapter);
+        
 
     }
 
