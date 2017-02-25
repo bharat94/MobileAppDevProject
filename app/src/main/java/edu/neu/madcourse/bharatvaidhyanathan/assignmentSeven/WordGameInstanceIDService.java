00f14c76ffe0e867339da7629bbe590a50146e41
Bharat V <vaidhyanathan.b@husky.neu.edu>
2017-02-25 00:53:25 -0500

    Added all the firebase codes and updated manifest and extra files

---
+++ app/src/main/java/edu/neu/madcourse/bharatvaidhyanathan/assignmentSeven/WordGameInstanceIDService.java
@@ -0,0 +1,11 @@
+package edu.neu.madcourse.bharatvaidhyanathan.assignmentSeven;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by vaidhyanathannarayanan on 25/02/17.
+ */
+
+public class WordGameInstanceIDService extends FirebaseInstanceIdService {
+
+}
---
+++ app/src/main/java/edu/neu/madcourse/bharatvaidhyanathan/assignmentSeven/WordGameMessagingService.java
@@ -0,0 +1,11 @@
+package edu.neu.madcourse.bharatvaidhyanathan.assignmentSeven;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by vaidhyanathannarayanan on 24/02/17.
+ */
+
+public class WordGameMessagingService extends FirebaseMessagingService {
+
+}
