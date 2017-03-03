4687422ef890119b4c46152a22cd6984f295bcdd
Bharat V <vaidhyanathan.b@husky.neu.edu>
2017-03-03 18:39:22 -0500

    implemented the communication module with user class for object passing

--- app/src/main/java/edu/neu/madcourse/bharatvaidhyanathan/assignmentSeven/User.java
+++ app/src/main/java/edu/neu/madcourse/bharatvaidhyanathan/assignmentSeven/User.java
@@ -5,4 +5,59 @@ package edu.neu.madcourse.bharatvaidhyanathan.assignmentSeven;
  */
 
 public class User {
+
+    String name;
+    String emailID;
+    String userID;
+    boolean isActive;
+    String regID;
+
+    public User(){
+        name = "";
+        emailID = "";
+        userID = "";
+        isActive = false;
+        regID = "";
+    }
+
+
+    public String getEmailID() {
+        return emailID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRegID() {
+        return regID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public void setEmailID(String emailID) {
+        this.emailID = emailID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRegID(String regID) {
+        this.regID = regID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
 }
